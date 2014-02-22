5bb34c17dc9eb6f0145937361f8824363e810589
Diego Pino Garcia <dpino@igalia.com>
2014-02-22 17:42:24 +0100

    Mostrar imagen diputado

--- www/app/helpers/application_helper.rb
+++ www/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  PATH_TO_IMAGES = "/data/img/"
+
+  def path_to_img(id_diputado, id_legislatura)
+    img_name = "#{id_diputado}_#{id_legislatura}.jpg"
+    return PATH_TO_IMAGES + img_name;
+  end
+
 end
