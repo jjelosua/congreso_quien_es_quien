0ddec1f470c76a7f3f63f7bc46c9c8e553793761
Javi <sanroman.javier@gmail.com>
2014-02-22 14:30:58 +0100

    test bbdd

--- www/app/controllers/diputados_controller.rb
+++ www/app/controllers/diputados_controller.rb
@@ -1,7 +1,10 @@
 class DiputadosController < ApplicationController
 
   def show
-    
+
+    @result = ActiveRecord::Base.connection.execute('SELECT count(id_legislatura) as count FROM comisiones')
+    @result = @result.first
+
   end
 
 end
