4cf9693895990e81da17779d4d43a4f3bef1298c
Javi <sanroman.javier@gmail.com>
2014-02-22 21:06:41 +0100

    css's

--- www/app/helpers/application_helper.rb
+++ www/app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 
   PATH_TO_IMAGES = "/data/img/"
 
-  def path_to_img(id_diputado, id_legislatura)
-    img_name = "#{id_diputado}_#{id_legislatura}.jpg"
+  def path_to_img(foto)
+    img_name = "#{foto}"
     return PATH_TO_IMAGES + img_name;
   end
 
