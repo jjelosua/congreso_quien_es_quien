26b581be6fba5c2be2ab5fdfa0a837bd79ec64e0
Diego Pino Garcia <dpino@igalia.com>
2014-02-22 17:26:36 +0100

    Mostrar detalle de diputado (primera legislatura)

--- www/app/controllers/diputados_controller.rb
+++ www/app/controllers/diputados_controller.rb
@@ -1,20 +1,12 @@
 class DiputadosController < ApplicationController
 
   def show
-
-    # @diputados = bd.diputados
-
-    # @diputados.each do |d|
-    #   p d.inspect
-    # end
-
     @legs = bd.legislaturas_diputado(params[:id])
 
-
-
-    # @result = ActiveRecord::Base.connection.execute('SELECT count(id_legislatura) as count FROM comisiones')
-    # @result = @result.first
-
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml { render :xml => @legs }
+    end
   end
 
 end
